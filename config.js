f95d5055f22ba52952449b070dae652102bd632e
Frank <benusfrank@gmail.com>
2017-04-24 14:58:47 +0200

    Cleaned up the config file

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // This is a configuration file, branche this file for any POC or demo org
-var oktaOrgUrl 			= 'https://dev-484122.oktapreview.com';						// Url of the org in Okta (full url)
-var appUrl 				= 'http://example.okta-demo.nl';							// The url provide by frank.benus@okta.com (please provide Okta API token when you request a url) 
-var apiProxyUrl 		= 'http://54.202.118.73/example-proxy.asp'					// The url for the API proxy, contact frank.benus@okta.com
-var oidcOktaAppId 		= '0oaa89kffsPd8FFXK0h7';									// Okta app id for OIDC app
-var oidcClientId 		= 'tFx9NRuwt7CbDX34D8KG';									// Client id for OIDC app
-var debugLogging		= true														// float the logs with debug data
+var oktaOrgUrl 			= '';									// Url of the org in Okta (full url)
+var appUrl 				= '';									// The url provide by frank.benus@okta.com (please provide Okta API token when you request a url) 
+var apiProxyUrl 		= ''									// The url for the API proxy, contact frank.benus@okta.com
+var oidcOktaAppId 		= '';									// Okta app id for OIDC app
+var oidcClientId 		= '';									// Client id for OIDC app
+var debugLogging		= true									// Clutter the console with debug data
 
 // BE AWARE: when set to true, this will cause changes in the Okta org
-var setupOrgInPreFlight = false;													// Create attributes, groups and policies for use cases automaticly
+var setupOrgInPreFlight = false;								// Create attributes, groups and policies for use cases automaticly
 
 // Use cases and features
 	//Self service and preferences
@@ -31,8 +31,8 @@ var setupOrgInPreFlight = false;													// Create attributes, groups and po
 	var useCustomColors = false;								// use the colors in the colors object, default is Okta theme (set param to false)
 	var colors = {												// Set hex color codes and enable custom colors, do not change the keys
 		'--background-color': 		'#FFFFFF',
-		'--color-primary': 			'#D62027', 
-		'--color-secundary': 		'#333333',
+		'--color-primary': 			'#ff8800', 
+		'--color-secundary': 		'#c5c5c5',
 		'--color-tertiary': 		'#F5F5F5',
 		'--text-color-content': 	'#333333',
 		'--text-color-header': 		'#FFFFFF',
@@ -44,8 +44,8 @@ var setupOrgInPreFlight = false;													// Create attributes, groups and po
 		'--message-success-text': 	'#FFFFFF'	
 	}
 	var label = {
-		'lbl_app_title': 				'Okta Example', 	// Page title
-		'lbl_app_name': 				'Example app'	 	// Main name of the app
+		'lbl_app_title': 				'App title', 			// Page title
+		'lbl_app_name': 				'Okta example app'	 	// Main name of the app
 	};
 
 // Settings for org
@@ -58,8 +58,3 @@ var setupOrgInPreFlight = false;													// Create attributes, groups and po
 	var sGroupAccountTrial 		= 'Portal: Trial accounts';					// Group with trial accounts
 	var sGroupMfaPrefered		= 'Portal: Prefer MFA';						// Group with users that want MFA sign-on policy
 	var sGroupHighRiskFeatures	= 'Portal: Enable high risk features';		// Group with users that enabled high risk features
-
-
-// Todo on preflight:
-// Check MFA policies for MFA options
-// Check for group rules
